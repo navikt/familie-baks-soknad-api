21b7e69d7346d8b850bda6be704aa1439f723762
Martine Enger <marenger@student.matnat.uio.no>
2020-06-22 11:40:06 +0200

    Ta PDL i egen PR pga gateway-åpning

--- src/main/kotlin/no/nav/familie/ba/soknad/api/helsesjekk/HelsesjekkController.kt
+++ src/main/kotlin/no/nav/familie/ba/soknad/api/helsesjekk/HelsesjekkController.kt
@@ -1,7 +1,6 @@
 package no.nav.familie.ba.soknad.api.helsesjekk
 
 import no.nav.familie.ba.soknad.api.integrasjoner.MottakClient
-import no.nav.familie.ba.soknad.api.integrasjoner.PdlClient
 import no.nav.familie.kontrakter.felles.Ressurs
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/helse")
-class HelsesjekkController(private val mottakClient: MottakClient, private val pdlClient: PdlClient) {
+class HelsesjekkController(private val mottakClient: MottakClient) {
 
     @GetMapping("soknad-api")
     fun pingApi(): Ressurs<String> {
@@ -18,15 +17,7 @@ class HelsesjekkController(private val mottakClient: MottakClient, private val p
 
     @GetMapping("pdl")
     fun pingPdl(): Ressurs<String> {
-        return Result.runCatching { pdlClient.ping() }
-                .fold(
-                        onSuccess = {
-                            Ressurs.success("Ping mot PDL-API OK")
-                        },
-                        onFailure = {
-                            Ressurs.failure(errorMessage = "Ping mot PDL-API feilet", error = it)
-                        }
-                )
+        return Ressurs.success("OK")
     }
 
     @GetMapping("mottak")
