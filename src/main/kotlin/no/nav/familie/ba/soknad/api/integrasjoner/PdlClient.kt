9be2d27064fae61f1b198ce8018f445b24eaba56
Martine Enger <marenger@student.matnat.uio.no>
2020-06-24 10:30:21 +0200

    Gjør options-kall for å pinge

--- src/main/kotlin/no/nav/familie/ba/soknad/api/integrasjoner/PdlClient.kt
+++ src/main/kotlin/no/nav/familie/ba/soknad/api/integrasjoner/PdlClient.kt
@@ -6,20 +6,23 @@ import no.nav.familie.http.client.Pingable
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestOperations
+import org.springframework.web.client.exchange
 import java.lang.IllegalStateException
 import java.net.URI
 
 @Component
 class PdlClient(@Value("\${PDL_API_URL}") private val pdlBaseUrl: String,
-                @Qualifier("restKlientMedApiKey") restOperations: RestOperations)
+                @Qualifier("restKlientMedApiKey") val restOperations: RestOperations)
     : AbstractRestClient(restOperations, "integrasjon"), Pingable {
 
     override fun ping() {
         val uri = URI.create("$pdlBaseUrl/graphql")
         try {
-            getForEntity<JsonNode>(uri)
+            //getForEntity<JsonNode>(uri)
+            restOperations.exchange<JsonNode>(uri, HttpMethod.OPTIONS)
             LOG.debug("Ping mot PDL-API OK")
         } catch (e: Exception) {
             LOG.warn("Ping mot PDL-API feilet")
