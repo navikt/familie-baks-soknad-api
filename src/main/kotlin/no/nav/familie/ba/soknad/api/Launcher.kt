4b0514fa2c84e0278826e00ef03ce7a9847c9f24
idaame <ida.enholm@me.com>
2020-06-26 15:05:23 +0200

    sett opp autentiseringskontroller

--- src/main/kotlin/no/nav/familie/ba/soknad/api/Launcher.kt
+++ src/main/kotlin/no/nav/familie/ba/soknad/api/Launcher.kt
@@ -1,10 +1,12 @@
 package no.nav.familie.ba.soknad.api
 
 import no.nav.familie.ba.soknad.api.config.ApplicationConfig
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication(scanBasePackages = ["no.nav.familie.ba.soknad.api"])
+@EnableJwtTokenValidation
 class Launcher
 
 fun main(args: Array<String>) {
--- src/test/kotlin/no/nav/familie/ba/soknad/api/DevLauncher.kt
+++ src/test/kotlin/no/nav/familie/ba/soknad/api/DevLauncher.kt
@@ -1,9 +1,11 @@
 package no.nav.familie.ba.soknad.api
 
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication(scanBasePackages = ["no.nav.familie.ba.soknad.api"])
+@EnableJwtTokenValidation
 class DevLauncher
 
 fun main(args: Array<String>) {
