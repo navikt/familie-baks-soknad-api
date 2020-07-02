dd90aaed3e263fd361378585ac304c421218a58f
jonatan <jonaengs@gmail.com>
2020-07-02 12:06:00 +0200

    legg til flere graphql-responser. skriv ferdig graphql-tester. Legg til hentNavn og hentPersoninfo mocker

--- src/test/kotlin/no/nav/familie/ba/soknad/api/integrasjoner/ClientMocks.kt
+++ src/test/kotlin/no/nav/familie/ba/soknad/api/integrasjoner/ClientMocks.kt
@@ -4,7 +4,8 @@ import io.mockk.Runs
 import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
-import no.nav.familie.ba.soknad.api.personopplysning.PdlClient
+import no.nav.familie.ba.soknad.api.personopplysning.*
+import org.assertj.core.groups.Tuple
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Profile
@@ -27,7 +28,28 @@ class ClientMocks {
     @Profile("mock-pdl")
     fun mockPdlClient(): PdlClient {
         val mockPdlClient = mockk<PdlClient>()
+
+        val voksenNavn = PdlNavn("Voksen", etternavn = "Voksnessen")
+        val relasjonBarn = PdlFamilierelasjon("12345678987", FAMILIERELASJONSROLLE.BARN)
+        val relasjonMor = PdlFamilierelasjon("12345678989", FAMILIERELASJONSROLLE.MOR)
+        val barneNavn = PdlNavn("Barn", etternavn = "Barnessen")
+
         every { mockPdlClient.ping() } just Runs
+        every { mockPdlClient.hentNavnOgRelasjoner(any()) } returns PdlHentPersonResponse(
+                data = PdlPerson(person = PdlPersonData(
+                        navn = listOf(voksenNavn),
+                        familierelasjoner = listOf(relasjonBarn, relasjonMor)
+                )),
+                errors = null
+        )
+        every { mockPdlClient.hentNavn(any()) } returns PdlHentPersonResponse(
+                data = PdlPerson(person = PdlPersonData(
+                        navn = listOf(barneNavn),
+                        familierelasjoner = emptyList()
+                )),
+                errors = null
+        )
         return mockPdlClient
     }
+
 }
--- src/test/kotlin/no/nav/familie/ba/soknad/api/personopplysning/pdlGraphqlTest.kt
+++ src/test/kotlin/no/nav/familie/ba/soknad/api/personopplysning/pdlGraphqlTest.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import java.io.File
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 
@@ -15,9 +16,27 @@ class PdlGraphqlTest {
     fun testDeserialization() {
         val resp = mapper.readValue(File(getFile("pdl/pdlPersonUtenRelasjoner.json")), PdlHentPersonResponse::class.java)
 
-
         assertEquals("ENGASJERT", resp.data.person!!.navn.first().fornavn)
         assertEquals("FYR", resp.data.person!!.navn.first().fornavn)
+        assertEquals(emptyList<PdlFamilierelasjon>(), resp.data.person!!.familierelasjoner)
+    }
+
+    @Test
+    fun testDeserializationOfResponseWithErrors() {
+        val resp = mapper.readValue(File(getFile("pdl/pdlPersonIkkeFunnetResponse.json")), PdlHentPersonResponse::class.java)
+        assertTrue(resp.harFeil())
+        assertTrue(resp.errorMessages().contains("Fant ikke person"))
+        assertTrue(resp.errorMessages().contains("Ikke tilgang"))
+    }
+
+    @Test  // Stjålet nesten ordrett fra familie-integrasjon: PdlGraphqlTest.
+    fun testFulltNavn() {
+        assertEquals(
+                "For Mellom Etter",
+                PdlNavn(fornavn = "For", mellomnavn = "Mellom", etternavn = "Etter").fulltNavn())
+        assertEquals(
+                "For Etter",
+                PdlNavn(fornavn = "For", etternavn = "Etter").fulltNavn())
     }
 
     private fun getFile(name: String): String {
