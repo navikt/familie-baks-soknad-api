0cb7c1cb32e53ae80898f1d3eee3cbf37d202138
jonatan <jonaengs@gmail.com>
2020-07-07 10:44:47 +0200

    slutt å bruke mock-client for pdl. lag testInterceptorklasse med interceptmetode som logger relevant data

--- src/main/kotlin/no/nav/familie/ba/soknad/api/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/soknad/api/config/ApplicationConfig.kt
@@ -5,6 +5,8 @@ import no.nav.familie.http.interceptor.ConsumerIdClientInterceptor
 import no.nav.familie.http.interceptor.MdcValuesPropagatingClientInterceptor
 import no.nav.familie.kontrakter.felles.objectMapper
 import no.nav.familie.log.filter.LogFilter
+import no.nav.security.token.support.core.context.TokenValidationContextHolder
+import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringBootConfiguration
@@ -14,7 +16,10 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Primary
+import org.springframework.http.HttpRequest
+import org.springframework.http.client.ClientHttpRequestExecution
 import org.springframework.http.client.ClientHttpRequestInterceptor
+import org.springframework.http.client.ClientHttpResponse
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestOperations
 import java.net.URI
@@ -50,20 +55,47 @@ internal class ApplicationConfig {
                 .build()
     }
 
+    @Bean
+    fun testInterceptor(@Value("\${PDL_API_APIKEY}") pdlApiKey: String,
+                                         @Value("\${PDL_API_URL}") pdlBaseUrl: String): ClientHttpRequestInterceptor {
+        return TestInterceptor()
+    }
+
     @Bean("restKlientMedApiKey")
     fun restTemplateMedApiKey(consumerIdClientInterceptor: ConsumerIdClientInterceptor,
-                              apiKeyInjectingClientInterceptor: ClientHttpRequestInterceptor): RestOperations {
+                              apiKeyInjectingClientInterceptor: ClientHttpRequestInterceptor,
+                                testInterceptor: ClientHttpRequestInterceptor): RestOperations {
         return RestTemplateBuilder()
                 .interceptors(consumerIdClientInterceptor,
                               apiKeyInjectingClientInterceptor,
+                              testInterceptor,
                               MdcValuesPropagatingClientInterceptor())
                 .additionalMessageConverters(MappingJackson2HttpMessageConverter(objectMapper))
                 .build()
     }
+    companion object {
+        val log = LoggerFactory.getLogger(ApplicationConfig::class.java)
+        const val pakkenavn = "no.nav.familie.ba.soknad.api"
+        private const val apiKeyHeader = "x-nav-apiKey"
+    }
+}
+
+class TestInterceptor: ClientHttpRequestInterceptor {
+
+    override fun intercept(request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution): ClientHttpResponse {
+        val contextHolder = SpringTokenValidationContextHolder()
+
+        log.info("\nFØR")
+        log.info(request.headers.toString())
+        log.info(body.toString(Charsets.UTF_8))
+        log.info(contextHolder.tokenValidationContext.getClaims("selvbetjening").allClaims.toString())
+        log.info(contextHolder.tokenValidationContext.getJwtToken("selvbetjening").tokenAsString)
+        log.info(contextHolder.tokenValidationContext.issuers.toString())
+        log.info("ETTER")
+        return execution.execute(request, body)
+    }
 
     companion object {
         private val log = LoggerFactory.getLogger(ApplicationConfig::class.java)
-        const val pakkenavn = "no.nav.familie.ba.soknad.api"
-        private const val apiKeyHeader = "x-nav-apiKey"
     }
 }
