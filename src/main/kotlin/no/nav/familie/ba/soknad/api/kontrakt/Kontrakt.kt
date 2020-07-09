ff8002925b16aec4fb4fced18c2ce774adf1a68b
jonatan <jonaengs@gmail.com>
2020-07-09 16:54:40 +0200

    gjør om søknadstype til enum. Logg kontrakt

--- src/main/kotlin/no/nav/familie/ba/soknad/api/kontrakt/Kontrakt.kt
+++ src/main/kotlin/no/nav/familie/ba/soknad/api/kontrakt/Kontrakt.kt
@@ -3,6 +3,13 @@ package no.nav.familie.ba.soknad.api.kontrakt
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class Kontrakt (
-        val søknadstype: String
+data class Kontrakt(
+        val søknadstype: Søknadstype
 )
+
+enum class Søknadstype {
+    IKKE_SATT,
+    ORDINÆR,
+    UTVIDET,
+    EØS,
+}
--- src/main/kotlin/no/nav/familie/ba/soknad/api/kontrakt/KontraktController.kt
+++ src/main/kotlin/no/nav/familie/ba/soknad/api/kontrakt/KontraktController.kt
@@ -1,6 +1,8 @@
 package no.nav.familie.ba.soknad.api.kontrakt
 
+import no.nav.familie.ba.soknad.api.config.ApplicationConfig
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -9,12 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping(path = ["/api"], produces = [MediaType.TEXT_PLAIN_VALUE])
 @ProtectedWithClaims(issuer = "selvbetjening", claimMap = ["acr=Level4"])
-@RequestMapping(path = ["/api"])
 class KontraktController() {
 
     @PostMapping("/kontrakt")
     fun kontraktInfo(@RequestBody(required=true) kontrakt: Kontrakt): String {
+        val log = LoggerFactory.getLogger(ApplicationConfig::class.java)
+        log.info(kontrakt.toString())
         return "Kontrakt OK"
     }
 }
