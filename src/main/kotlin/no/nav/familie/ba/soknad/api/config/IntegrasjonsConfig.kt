2b54bfbe3a301834f901b6670ad77dfa4a4e8d0e
Øyvind Hagen <oyvind.hagen@nav.no>
2020-07-13 09:35:41 +0200

    Legger apikey paa stsclient.

--- src/main/kotlin/no/nav/familie/ba/soknad/api/config/IntegrasjonsConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/soknad/api/config/IntegrasjonsConfig.kt
@@ -14,9 +14,10 @@ class IntegrasjonsConfig {
     fun stsRestClient(
             @Value("\${STS_URL}") stsUrl: URI,
             @Value("\${CREDENTIAL_USERNAME}") stsUsername: String,
-            @Value("\${CREDENTIAL_PASSWORD}") stsPassword: String): StsRestClient {
+            @Value("\${CREDENTIAL_PASSWORD}") stsPassword: String,
+            @Value("\${STS_APIKEY}") stsApiKey: String): StsRestClient {
 
         val stsFullUrl = URI.create("$stsUrl?grant_type=client_credentials&scope=openid")
-        return StsRestClient(objectMapper, stsFullUrl, stsUsername, stsPassword)
+        return StsRestClient(objectMapper, stsFullUrl, stsUsername, stsPassword, stsApiKey)
     }
 }
