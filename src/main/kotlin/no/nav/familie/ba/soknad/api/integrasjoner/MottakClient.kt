379f9f6c26e0f3391af8c41ecc29a4125439faf9
idaame <ida.enholm@me.com>
2020-07-15 14:40:03 +0200

    legg til multipartFormdata

--- src/main/kotlin/no/nav/familie/ba/soknad/api/integrasjoner/MottakClient.kt
+++ src/main/kotlin/no/nav/familie/ba/soknad/api/integrasjoner/MottakClient.kt
@@ -3,9 +3,9 @@ package no.nav.familie.ba.soknad.api.integrasjoner
 import com.fasterxml.jackson.databind.JsonNode
 import main.kotlin.no.nav.familie.ba.søknad.Søknad
 import no.nav.familie.http.client.AbstractRestClient
+import no.nav.familie.http.client.MultipartBuilder
 import no.nav.familie.http.client.Pingable
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestOperations
@@ -29,9 +29,11 @@ class MottakClient(@Value("\${FAMILIE_BA_MOTTAK_URL}") private val mottakBaseUrl
     }
 
     fun sendSøknad(søknad: Søknad): String {
-        val uri = URI.create("$mottakBaseUrl/internal/e2e/soeknad")
+        val uri: URI = URI.create("$mottakBaseUrl/api/soknad")
         try {
-            val response = postForEntity<String>(uri, søknad)
+            val multipartBuilder = MultipartBuilder().withJson("søknad", søknad)
+
+            val response = postForEntity<String>(uri = uri, payload = multipartBuilder.build(), httpHeaders = MultipartBuilder.MULTIPART_HEADERS)
             LOG.debug("Sende søknad til mottak OK")
             return response
         } catch (e: Exception) {
