9d9ace73296bbb233ccd4a534d4f3505134938c7
Bjørn Snoen <bjorn.magne.snoen@nav.no>
2021-01-25 18:34:50 +0100

    (feat) Håndtering av partially staged filer i commit-hook

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -1,5 +1,40 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+BLINK='\033[33;5;7m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
 
 staged=$(git diff --name-only --cached)
+unstaged=$(git diff --name-only)
+
+
+for file in $staged; do
+  if containsElement "$file" "${unstaged[@]}"; then
+    echo " "
+    echo -e "========== ${BLINK}Partially staged file detected, we won't be able to automatically format and restage. Running check only.${NC} =========="
+    echo " "
+
+    mvn antrun:run@ktlint -q
+    retval=$?
+    if [ $retval -ne 0 ]; then
+      echo " "
+      echo -e "========== ktlint check failed. Run ${YELLOW}mvn antrun:run@ktlint-format${NC} and re-stage the file before committing again =========="
+      echo " "
+
+      exit $retval
+    fi
+    # No need to run check for every file, time to leave the loop
+    break
+  fi
+done
+
+# Subshell because for some reason we can't exit otherwise
+set -ex
 mvn antrun:run@ktlint-format
-for file in $staged; do git add $file; done
