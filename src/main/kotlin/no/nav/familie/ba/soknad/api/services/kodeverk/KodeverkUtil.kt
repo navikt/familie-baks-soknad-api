9b8c688d6929d56571a1552353d48129c698d8c2
Silje Risvik <silje.risvik@nav.no>
2021-03-23 13:35:54 +0100

    Fikset client

---
+++ src/main/kotlin/no/nav/familie/ba/soknad/api/services/kodeverk/KodeverkUtil.kt
@@ -0,0 +1,17 @@
+package no.nav.familie.ba.soknad.api.services.pdl.kodeverk
+
+import no.nav.familie.kontrakter.felles.kodeverk.KodeverkDto
+import no.nav.familie.kontrakter.felles.kodeverk.KodeverkSpråk
+import no.nav.familie.kontrakter.felles.kodeverk.hentGjeldende
+
+fun KodeverkDto.mapTerm(): Map<String, String> {
+    return betydninger.mapValues {
+        if (it.value.isEmpty()) {
+            ""
+        } else if (it.value.size != 1) {
+            this.hentGjeldende(it.key) ?: ""
+        } else {
+            it.value.singleOrNull()?.beskrivelser?.get(KodeverkSpråk.BOKMÅL.kode)?.term ?: ""
+        }
+    }
+}
