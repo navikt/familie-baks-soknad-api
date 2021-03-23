2ac3191f9b4db7e52d5281e55697abca68a06ac2
Silje Risvik <silje.risvik@nav.no>
2021-03-23 13:22:09 +0100

    fix

--- src/main/kotlin/no/nav/familie/ba/soknad/api/clients/kodeverk/KodeverkClient.kt
+++ src/main/kotlin/no/nav/familie/ba/soknad/api/clients/kodeverk/KodeverkClient.kt
@@ -1,46 +1,31 @@
 package no.nav.familie.ba.soknad.api.clients.kodeverk
 
+import java.net.URI
 import no.nav.familie.http.client.AbstractPingableRestClient
+import no.nav.familie.http.client.Pingable
 import no.nav.familie.http.util.UriUtil
 import no.nav.familie.kontrakter.felles.kodeverk.KodeverkDto
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestOperations
-import java.net.URI
-
 
 @Component
-class KodeverkClient(@Value("\${KODEVERK_URL}") private val kodeverkUri: URI,
-                     @Qualifier("sts") private val restTemplate: RestOperations)
-    : AbstractPingableRestClient(restTemplate, "kodeverk") {
+class KodeverkClient(
+    @Value("\${KODEVERK_URL}") private val kodeverkBaseUrl: String,
+    @Qualifier("restKlientMedApiKey") private val restOperations: RestOperations
+) :
+    AbstractPingableRestClient(restOperations, "kodeverk"), Pingable {
 
-    override val pingUri: URI = UriUtil.uri(kodeverkUri, PATH_PING)
+    private val kodeverkUri: URI = URI.create(kodeverkBaseUrl)
 
     fun hentPostnummer(): KodeverkDto {
         return getForEntity(kodeverkUri("Postnummer"))
     }
 
-    fun hentPostnummerMedHistorikk(): KodeverkDto {
-        return getForEntity(kodeverkUri("Postnummer", true))
-    }
-
-    fun hentLandkoder(): KodeverkDto {
-        return getForEntity(kodeverkUri("Landkoder"))
-    }
-
-    fun hentLandkoderMedHistorikk(): KodeverkDto {
-        return getForEntity(kodeverkUri("Landkoder", true))
-    }
-
-    fun kodeverkUri(kodeverksnavn: String,
-                    medHistorikk: Boolean = false): URI {
+    fun kodeverkUri(kodeverksnavn: String, medHistorikk: Boolean = false): URI {
         val query = if (medHistorikk) QUERY_MED_HISTORIKK else QUERY
         return UriUtil.uri(kodeverkUri, "api/v1/kodeverk/$kodeverksnavn/koder/betydninger", query)
-    }
-
-    fun hentEEALandkoder(): KodeverkDto {
-        return getForEntity(kodeverkUri("EEAFreg", medHistorikk = true))
     }
 
     companion object {
@@ -48,4 +33,6 @@ class KodeverkClient(@Value("\${KODEVERK_URL}") private val kodeverkUri: URI,
         private const val QUERY = "ekskluderUgyldige=true&spraak=nb"
         private const val QUERY_MED_HISTORIKK = "ekskluderUgyldige=false&spraak=nb"
     }
+
+    override val pingUri: URI = UriUtil.uri(kodeverkUri, PATH_PING)
 }
