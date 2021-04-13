597e088d335d8c5eefd69fd557411e27870cb3b6
Silje Risvik <silje.risvik@nav.no>
2021-04-13 13:30:54 +0200

    kodeverk url

--- src/main/kotlin/no/nav/familie/ba/soknad/api/clients/kodeverk/KodeverkClient.kt
+++ src/main/kotlin/no/nav/familie/ba/soknad/api/clients/kodeverk/KodeverkClient.kt
@@ -25,7 +25,7 @@ class KodeverkClient(
 
     fun kodeverkUri(kodeverksnavn: String, medHistorikk: Boolean = false): URI {
         val query = if (medHistorikk) QUERY_MED_HISTORIKK else QUERY
-        return UriUtil.uri(kodeverkUri, "api/v1/kodeverk/$kodeverksnavn/koder/betydninger", query)
+        return UriUtil.uri(kodeverkUri, "/kodeverk/api/v1/kodeverk/$kodeverksnavn/koder/betydninger", query)
     }
 
     companion object {
