dec9227f7f553c749fbe731bbb32742de071f7c1
Bjørn Snoen <77009351+bjornsnoen-nav@users.noreply.github.com>
2021-06-16 10:03:48 +0200

    (fix) Returner ressurs fra /innlogget endpoint (#67)

--- src/main/kotlin/no/nav/familie/ba/soknad/api/autentisering/AutentiseringController.kt
+++ src/main/kotlin/no/nav/familie/ba/soknad/api/autentisering/AutentiseringController.kt
@@ -1,23 +1,25 @@
 package no.nav.familie.ba.soknad.api.autentisering
 
 import io.micrometer.core.instrument.Metrics
+import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping(path = ["/api"], produces = [MediaType.TEXT_PLAIN_VALUE])
+@RequestMapping(path = ["/api"], produces = [MediaType.APPLICATION_JSON_VALUE])
 @ProtectedWithClaims(issuer = "selvbetjening", claimMap = ["acr=Level4"])
 class AutentiseringController {
 
     val innlogget = Metrics.counter("innlogget")
 
     @GetMapping("/innlogget")
-    fun verifiserAutentisering(): String {
+    fun verifiserAutentisering(): ResponseEntity<Ressurs<String>> {
         innlogget.increment()
 
-        return "Autentisert kall"
+        return ResponseEntity.ok(Ressurs.success("Autentisert kall"))
     }
 }
