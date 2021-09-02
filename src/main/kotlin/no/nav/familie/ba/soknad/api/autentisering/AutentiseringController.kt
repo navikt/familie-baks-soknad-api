85700d1eb3e54fb491cf32e68f83d31dca766b57
Daphne Leebeek <daphne.leebeek@nav.no>
2021-09-02 09:09:37 +0200

    Fjern tom linje

--- src/main/kotlin/no/nav/familie/ba/soknad/api/autentisering/AutentiseringController.kt
+++ src/main/kotlin/no/nav/familie/ba/soknad/api/autentisering/AutentiseringController.kt
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-
 @RestController
 @RequestMapping(path = ["/api"], produces = [MediaType.APPLICATION_JSON_VALUE])
 @ProtectedWithClaims(issuer = "selvbetjening", claimMap = ["acr=Level4"])
