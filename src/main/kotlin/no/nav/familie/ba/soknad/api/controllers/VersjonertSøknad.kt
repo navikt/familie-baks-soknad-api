888ff8957f90f87aef2c9b644c1cfffaa9aaa737
André Wolden <wolden.andre@gmail.com>
2022-01-24 10:14:38 +0100

    fjerner VersjonertSøknad sealed class, fordi den ikke trengs

--- src/main/kotlin/no/nav/familie/ba/soknad/api/controllers/VersjonertSøknad.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.familie.ba.soknad.api.controllers
-
-import no.nav.familie.kontrakter.ba.søknad.v6.Søknad
-
-sealed class VersjonertSøknad
-
-data class SøknadV6(val søknad: Søknad) : VersjonertSøknad()
-data class SøknadV7(val søknad: SøknadNewWip) : VersjonertSøknad()
