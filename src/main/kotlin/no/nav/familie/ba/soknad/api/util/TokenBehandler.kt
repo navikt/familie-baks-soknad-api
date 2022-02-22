eba7f647ab704a58195335cd3d1f1900b9604325
Daphne Leebeek <daphne.leebeek@nav.no>
2022-02-22 08:58:05 +0100

    Fiks lint

--- src/main/kotlin/no/nav/familie/ba/soknad/api/util/TokenBehandler.kt
+++ src/main/kotlin/no/nav/familie/ba/soknad/api/util/TokenBehandler.kt
@@ -3,6 +3,7 @@ package no.nav.familie.ba.soknad.api.util
 import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
 
 object TokenBehandler {
+
     private const val ISSUER = "selvbetjening"
 
     fun hentToken(): String {
@@ -14,7 +15,7 @@ object TokenBehandler {
         val contextHolder = SpringTokenValidationContextHolder()
         val claims = contextHolder.tokenValidationContext.getClaims(ISSUER)
         return claims.getStringClaim("pid")
-               ?: claims.getStringClaim("sub")
-               ?: error("Finner ikke sub/pid på token")
+           ?: claims.getStringClaim("sub")
+           ?: error("Finner ikke sub/pid på token")
     }
 }
