7a6b4489fd8fd9763eab8461c4db0e7709065b3b
Daphne Leebeek <daphne.leebeek@nav.no>
2022-02-28 14:35:34 +0100

    Legg til triggetEøs på personer i kontrakt

--- src/main/kotlin/no/nav/familie/ba/soknad/api/controllers/SøknadNewWip.kt
+++ src/main/kotlin/no/nav/familie/ba/soknad/api/controllers/SøknadNewWip.kt
@@ -30,6 +30,7 @@ data class SøknadNewWip(
 )
 
 data class Søker(
+    val triggetEøs: Boolean,
     val ident: Søknadsfelt<String>,
     val navn: Søknadsfelt<String>,
     val statsborgerskap: Søknadsfelt<List<String>>,
@@ -75,6 +76,7 @@ data class Utbetalingsperiode(
 )
 
 data class Barn(
+    val triggetEøs: Boolean,
     val ident: Søknadsfelt<String>,
     val navn: Søknadsfelt<String>,
     val registrertBostedType: Søknadsfelt<RegistrertBostedType>,
