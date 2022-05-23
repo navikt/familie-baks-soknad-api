818365dd99aa4c1f02200933f2e3c6633210d2cb
norafutsaeter <nora.futseter@nav.no>
2022-05-23 10:46:04 +0200

    wip ny kontrakt

---
+++ src/main/kotlin/no/nav/familie/ba/soknad/api/controllers/SøknadWipV8.kt
@@ -0,0 +1,66 @@
+package no.nav.familie.ba.soknad.api.controllers
+import no.nav.familie.kontrakter.ba.søknad.SIVILSTANDTYPE
+import no.nav.familie.kontrakter.ba.søknad.SøknadAdresse
+import no.nav.familie.kontrakter.ba.søknad.v4.Locale
+import no.nav.familie.kontrakter.ba.søknad.v4.NåværendeSamboer
+import no.nav.familie.kontrakter.ba.søknad.v4.SpørsmålId
+import no.nav.familie.kontrakter.ba.søknad.v4.Søknaddokumentasjon
+import no.nav.familie.kontrakter.ba.søknad.v4.Søknadsfelt
+import no.nav.familie.kontrakter.ba.søknad.v4.Søknadstype
+import no.nav.familie.kontrakter.ba.søknad.v4.TidligereSamboer
+import no.nav.familie.kontrakter.ba.søknad.v4.Utenlandsopphold
+import no.nav.familie.kontrakter.ba.søknad.v7.Arbeidsperiode
+import no.nav.familie.kontrakter.ba.søknad.v7.Utbetalingsperiode
+import no.nav.familie.kontrakter.ba.søknad.v7.Pensjonsperiode
+import no.nav.familie.kontrakter.ba.søknad.v7.IdNummer
+import no.nav.familie.kontrakter.ba.søknad.v7.Barn
+
+data class SøknadWipV8(
+    val kontraktVersjon: Int,
+    val antallEøsSteg: Int,
+    val søknadstype: Søknadstype,
+    val søker: Søker,
+    val barn: List<Barn>,
+    val spørsmål: Map<SpørsmålId, Søknadsfelt<Any>>,
+    val dokumentasjon: List<Søknaddokumentasjon>,
+    val teksterUtenomSpørsmål: Map<SpørsmålId, Map<Locale, String>>,
+    val originalSpråk: Locale
+)
+
+data class Søker(
+    val harEøsSteg: Boolean,
+    val ident: Søknadsfelt<String>,
+    val navn: Søknadsfelt<String>,
+    val statsborgerskap: Søknadsfelt<List<String>>,
+    //Ny
+    val adresse: Søknadsfelt<SøknadAdresse>? = null ,
+
+    val sivilstand: Søknadsfelt<SIVILSTANDTYPE>,
+    val spørsmål: Map<String, Søknadsfelt<Any>>,
+    val nåværendeSamboer: Søknadsfelt<NåværendeSamboer>?,
+    val tidligereSamboere: List<Søknadsfelt<TidligereSamboer>>,
+    val utenlandsperioder: List<Søknadsfelt<Utenlandsopphold>> = listOf(),
+    val andreUtbetalingsperioder: List<Søknadsfelt<Utbetalingsperiode>> = listOf(),
+    val arbeidsperioderUtland: List<Søknadsfelt<Arbeidsperiode>> = listOf(),
+    val arbeidsperioderNorge: List<Søknadsfelt<Arbeidsperiode>> = listOf(),
+    val pensjonsperioderNorge: List<Søknadsfelt<Pensjonsperiode>> = listOf(),
+    val pensjonsperioderUtland: List<Søknadsfelt<Pensjonsperiode>> = listOf(),
+    val idNummer: List<Søknadsfelt<IdNummer>> = listOf()
+)
+
+data class Omsorgsperson(
+    val navn: Søknadsfelt<String>,
+    val slektsforhold: Søknadsfelt<String>,
+    val slektsforholdSpesifisering: Søknadsfelt<String>,
+    val idNummer: Søknadsfelt<String>,
+    val adresse: Søknadsfelt<String>,
+    //Nye
+    val arbeidNorge:  Søknadsfelt<String?>,
+    val arbeidUtland: Søknadsfelt<String?>,
+    val arbeidsperioderUtland:List<Søknadsfelt<Arbeidsperiode>> = listOf(),
+    val arbeidsperioderNorge: List<Søknadsfelt<Arbeidsperiode>> = listOf(),
+    val pensjonUtland: Søknadsfelt<String?>,
+    val pensjonsperioderUtland:  List<Søknadsfelt<Pensjonsperiode>> = listOf(),
+    val pensjonNorge: Søknadsfelt<String?>,
+    val pensjonsperioderNorge:  List<Søknadsfelt<Pensjonsperiode>> = listOf(),
+)
