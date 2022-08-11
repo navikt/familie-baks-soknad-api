1894fdc83a338863e1fc5cca9497d643c3f6ce9a
Daphne <daphne.leebeek@nav.no>
2022-08-11 13:28:45 +0200

    Fix/tilgang ks soknad (#117)
    * Legg til ks-soknad i application rules
    
    * Fjern v7
    
    * Legg til endepunkt for kontantstøtte

--- src/main/kotlin/no/nav/familie/ba/soknad/api/controllers/SøknadController.kt
+++ src/main/kotlin/no/nav/familie/ba/soknad/api/controllers/SøknadController.kt
@@ -1,9 +1,10 @@
 package no.nav.familie.ba.soknad.api.controllers
 
+import java.time.LocalDateTime
 import no.nav.familie.ba.soknad.api.clients.mottak.MottakClient
+import no.nav.familie.ba.soknad.api.domene.KontantstøtteSøknad
 import no.nav.familie.ba.soknad.api.domene.Kvittering
 import no.nav.familie.ba.soknad.api.util.TokenBehandler
-import no.nav.familie.kontrakter.ba.søknad.v7.Søknad as SøknadV7
 import no.nav.familie.kontrakter.ba.søknad.v8.Søknad as SøknadV8
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
@@ -19,20 +20,6 @@ import org.springframework.web.bind.annotation.RestController
 @ProtectedWithClaims(issuer = "selvbetjening", claimMap = ["acr=Level4"])
 class SøknadController(private val mottakClient: MottakClient) {
 
-    @PostMapping("/soknad/v7")
-    fun søknadsmottakV7(@RequestBody(required = true) søknad: SøknadV7): ResponseEntity<Ressurs<Kvittering>> {
-
-        val søknadMedIdentFraToken = søknad.copy(
-            søker = søknad.søker.copy(
-                ident = søknad.søker.ident.copy(
-                    verdi = søknad.søker.ident.verdi.mapValues { TokenBehandler.hentFnr() }
-                )
-            )
-        )
-
-        return ResponseEntity.ok().body(mottakClient.sendSøknadV7(søknadMedIdentFraToken))
-    }
-
     @PostMapping("/soknad/v8")
     fun søknadsmottakV8(@RequestBody(required = true) søknad: SøknadV8): ResponseEntity<Ressurs<Kvittering>> {
 
@@ -45,5 +32,31 @@ class SøknadController(private val mottakClient: MottakClient) {
         )
 
         return ResponseEntity.ok().body(mottakClient.sendSøknadV8(søknadMedIdentFraToken))
+    }
+
+    @PostMapping("/soknad/kontantstotte")
+    fun søknadsmottakKontantStotte(
+        @RequestBody(required = true)
+        søknad: KontantstøtteSøknad
+    ): ResponseEntity<Ressurs<Kvittering>> {
+
+        val søknadMedIdentFraToken = søknad.copy(
+            søker = søknad.søker.copy(
+                ident = søknad.søker.ident.copy(
+                    verdi = søknad.søker.ident.verdi.mapValues { TokenBehandler.hentFnr() }
+                )
+            )
+        )
+
+        // Todo: legg inn integrasjon mot kontantstøttemottak
+        return ResponseEntity.ok()
+            .body(
+                Ressurs(
+                    data = Kvittering("suksess", LocalDateTime.now()),
+                    melding = "suksess",
+                    status = Ressurs.Status.SUKSESS,
+                    stacktrace = null
+                )
+            )
     }
 }
