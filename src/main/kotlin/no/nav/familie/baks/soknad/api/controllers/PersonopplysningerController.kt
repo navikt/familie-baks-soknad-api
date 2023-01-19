6121089aa1d5bcf01fcf2d4572d1b34e4e421ff4
bragejahren <70642183+bragejahren@users.noreply.github.com>
2023-01-19 14:38:44 +0100

    Henter kun barn med alder under aldersgrense for ytelse (#129)
    * Filtrerer ut barn med alder under aldersgrense for ytelse
    
    * Lagt til tester for kontantstøtte aldersgrense

--- src/main/kotlin/no/nav/familie/baks/soknad/api/controllers/PersonopplysningerController.kt
+++ src/main/kotlin/no/nav/familie/baks/soknad/api/controllers/PersonopplysningerController.kt
@@ -1,14 +1,17 @@
 package no.nav.familie.baks.soknad.api.controllers
 
 import no.nav.familie.baks.soknad.api.domene.Person
+import no.nav.familie.baks.soknad.api.domene.Ytelse
 import no.nav.familie.baks.soknad.api.services.pdl.PersonopplysningerService
 import no.nav.familie.baks.soknad.api.util.TokenBehandler
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -16,12 +19,26 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping(path = ["/api"], produces = [MediaType.APPLICATION_JSON_VALUE])
 class PersonopplysningerController(private val personopplysningerService: PersonopplysningerService) {
 
+    // TODO: Fjerne denne når ba-soknad og ks-soknad er oppdatert
     @PostMapping("/personopplysning")
     fun personInfo(): ResponseEntity<Ressurs<Person?>> {
         return ResponseEntity.ok(
             Ressurs.success(
                 personopplysningerService.hentPersoninfo(
-                    TokenBehandler.hentFnr()
+                    TokenBehandler.hentFnr(),
+                    Ytelse.BARNETRYGD
+                )
+            )
+        )
+    }
+
+    @GetMapping("/personopplysning")
+    fun personInfo(@RequestParam ytelse: Ytelse): ResponseEntity<Ressurs<Person?>> {
+        return ResponseEntity.ok(
+            Ressurs.success(
+                personopplysningerService.hentPersoninfo(
+                    TokenBehandler.hentFnr(),
+                    ytelse
                 )
             )
         )
--- src/main/kotlin/no/nav/familie/baks/soknad/api/services/pdl/PersonopplysningerService.kt
+++ src/main/kotlin/no/nav/familie/baks/soknad/api/services/pdl/PersonopplysningerService.kt
@@ -1,19 +1,20 @@
 package no.nav.familie.baks.soknad.api.services.pdl
 
-import java.time.LocalDate
-import java.time.Period
-import java.time.format.DateTimeFormatter
 import no.nav.familie.baks.soknad.api.clients.kodeverk.KodeverkClient
 import no.nav.familie.baks.soknad.api.clients.pdl.PdlApp2AppClient
 import no.nav.familie.baks.soknad.api.clients.pdl.PdlBrukerClient
 import no.nav.familie.baks.soknad.api.clients.pdl.PdlDoedsafall
 import no.nav.familie.baks.soknad.api.domene.Barn
 import no.nav.familie.baks.soknad.api.domene.Person
+import no.nav.familie.baks.soknad.api.domene.Ytelse
 import no.nav.familie.baks.soknad.api.services.kodeverk.CachedKodeverkService
 import no.nav.familie.baks.soknad.api.services.pdl.mapper.PdlBarnMapper
 import no.nav.familie.baks.soknad.api.services.pdl.mapper.PdlMapper
 import no.nav.familie.kontrakter.felles.personopplysning.Bostedsadresse
 import org.springframework.stereotype.Service
+import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
 
 @Service
 class PersonopplysningerService(
@@ -23,12 +24,13 @@ class PersonopplysningerService(
 ) {
 
     val kodeverkService = CachedKodeverkService(kodeverkClient)
-    fun hentPersoninfo(personIdent: String, somSystem: Boolean = false): Person {
+    fun hentPersoninfo(personIdent: String, ytelse: Ytelse, somSystem: Boolean = false): Person {
         val response = if (somSystem) pdlApp2AppClient.hentPerson(personIdent) else pdlClient.hentPerson(personIdent)
 
         val barnTilSoeker = hentBarnTilSoeker(
             fnrBarn = PdlMapper.mapFnrBarn(response.data.person!!.forelderBarnRelasjon),
-            sokerAdresse = response.data.person.bostedsadresse.firstOrNull()
+            sokerAdresse = response.data.person.bostedsadresse.firstOrNull(),
+            ytelse
         )
 
         return response.data.person.let {
@@ -36,12 +38,14 @@ class PersonopplysningerService(
         }
     }
 
-    fun hentBarnTilSoeker(fnrBarn: List<String>, sokerAdresse: Bostedsadresse?): Set<Barn> {
+    fun hentBarnTilSoeker(fnrBarn: List<String>, sokerAdresse: Bostedsadresse?, ytelse: Ytelse): Set<Barn> {
         return fnrBarn
             .map { ident -> pdlApp2AppClient.hentPerson(ident) }
             .filter {
-                erBarnILive(it.data.person?.doedsfall) &&
-                    erUnderAtten(parseIsoDato(it.data.person?.foedsel?.firstOrNull()?.foedselsdato))
+                erBarnILive(it.data.person?.doedsfall) && erBarnetsAlderUnderAldersgrenseForYtelse(
+                    parseIsoDato(it.data.person?.foedsel?.firstOrNull()?.foedselsdato),
+                    ytelse
+                )
             }
             .map { PdlBarnMapper.mapBarn(it, sokerAdresse) }.toSet()
     }
@@ -50,13 +54,12 @@ class PersonopplysningerService(
         return doedsfall?.firstOrNull()?.doedsdato == null
     }
 
-    fun erUnderAtten(fødselsdato: LocalDate?): Boolean {
+    fun erBarnetsAlderUnderAldersgrenseForYtelse(fødselsdato: LocalDate?, ytelse: Ytelse): Boolean {
         if (fødselsdato == null) {
             return false
         }
         val alder = Period.between(fødselsdato, LocalDate.now())
-        val alderIÅr = alder.years
-        return alderIÅr < 18
+        return alder.toTotalMonths() < ytelse.aldersgrense.toTotalMonths()
     }
 
     fun parseIsoDato(dato: String?): LocalDate? {
