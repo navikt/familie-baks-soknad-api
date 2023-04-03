4c727a72564e2c9c592a112abc879ffa0851499b
bragejahren <70642183+bragejahren@users.noreply.github.com>
2023-04-03 15:27:50 +0200

    Rydde i RestOperation beans (#145)
    * Ryddet litt i PdlClients
    
    * Bruker tokenExchange restOperations fremfor restKlientMottak i MottakClient

--- src/main/kotlin/no/nav/familie/baks/soknad/api/clients/pdl/PdlBrukerClient.kt
+++ src/main/kotlin/no/nav/familie/baks/soknad/api/clients/pdl/PdlBrukerClient.kt
@@ -2,19 +2,11 @@ package no.nav.familie.baks.soknad.api.clients.pdl
 
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestOperations
 
 @Component
 class PdlBrukerClient(
     @Value("\${PDL_URL}") private val pdlBaseUrl: String,
-    @Qualifier("tokenExchange") private val restOperations: RestOperations,
-) : PdlClient(pdlBaseUrl, restOperations) {
-
-    override fun httpHeaders(): HttpHeaders {
-        return HttpHeaders().apply {
-            add("Tema", TEMA)
-        }
-    }
-}
+    @Qualifier("tokenExchange") private val restOperations: RestOperations
+) : PdlClient(pdlBaseUrl, restOperations)
