f019d070f2dfc7e619781fd1cda1d8005579c3df
Hanne Olsen <hanne.olsen@nav.no>
2024-06-28 11:47:05 +0200

    Lager klient for å hente aktivt kontonummer fra kontoregisteret

---
+++ src/main/kotlin/no/nav/familie/baks/soknad/api/clients/kontoregister/KontoregisterClient.kt
@@ -0,0 +1,36 @@
+package no.nav.familie.baks.soknad.api.clients.mottak
+
+import no.nav.familie.http.client.AbstractRestClient
+import no.nav.familie.http.util.UriUtil
+import no.nav.familie.kontrakter.felles.Ressurs
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestOperations
+import java.net.URI
+
+@Component
+class KontoregisterClient(
+    @Value("\${KONTOREGISTER_URL}") private val kontoregisterBaseUrl: String,
+    @Qualifier("tokenExchange") private val restOperations: RestOperations
+) :
+    AbstractRestClient(restOperations, "kontoregister") {
+    fun hentKontonummer(kontohaver: KontoregisterRequestDto): Ressurs<KontoregisterResponseDto> {
+        val uri: URI = UriUtil.uri(URI.create(kontoregisterBaseUrl), "hent-aktiv-konto")
+        return postForEntity<Ressurs<KontoregisterResponseDto>>(
+            uri = uri,
+            payload = kontohaver
+        )
+    }
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(MottakClient::class.java)
+    }
+}
+
+data class KontoregisterRequestDto(val kontohaver: String)
+
+data class KontoregisterResponseDto(val kontonummer: String, val utenlandskKontoInfo: UtenlandskKontoInfo)
+
+data class UtenlandskKontoInfo(val banknavn: String, val bankkode: String, val bankLandkode: String)
