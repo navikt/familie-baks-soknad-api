07539b10fb859f2da000db530f0f0190499fd432
Hanne Olsen <hanne.olsen@nav.no>
2024-08-12 13:32:10 +0200

    Integrere mot Kontoregisteret (#259)
    ### 💰 Hva forsøker du å løse i denne PR'en
    Integrasjon mot Kontoregisteret for å hente ut aktiv konto for bruker
    via TokenX. Her er deres
    [swagger-doumentasjon](https://sokos-kontoregister-person.intern.dev.nav.no/api/borger/v1/docs/#/kontoregister.v1/hentAktivKonto)
    på endepunktet.
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    _Jeg har ikke skrevet tester fordi:_
    Så på det sammen med @stigebil, det fantes ikke et godt oppsett for å
    skrive integrasjonstester. Dette er kanskje noe vi må endre på fremover,
    men ikke i denne PRen.
    
    ### 🤷‍♀ ️Hvor er det lurt å starte?
    Commitene er rimelig strukturerte, så det gir kanskje mest mening å gå
    gjennom de commit for commit 😊
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [X] Nei

---
+++ src/test/kotlin/no/nav/familie/baks/soknad/api/kontoregister/KontoregisterTestConfig.kt
@@ -0,0 +1,27 @@
+package no.nav.familie.baks.soknad.api.kontoregister
+
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.familie.baks.soknad.api.clients.mottak.KontoregisterClient
+import no.nav.familie.baks.soknad.api.clients.mottak.KontoregisterResponseDto
+import no.nav.familie.kontrakter.felles.Ressurs
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.Profile
+
+@Configuration
+class KontoregisterTestConfig {
+    @Bean
+    @Profile("mock-kontoregister")
+    @Primary
+    fun kontoregisterClientMock(): KontoregisterClient {
+        val kontoregisterClient: KontoregisterClient = mockk()
+        val kontoregisterResponse =
+            KontoregisterResponseDto(
+                "815.493.00"
+            )
+        every { kontoregisterClient.hentKontonummer(any()) } returns Ressurs.success(kontoregisterResponse)
+        return kontoregisterClient
+    }
+}
