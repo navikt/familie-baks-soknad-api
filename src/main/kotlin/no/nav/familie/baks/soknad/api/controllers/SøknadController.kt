31744b925c07242aae451a1546c3e126ffb71aef
Henning Solberg <henning.solberg@nav.no>
2025-04-03 08:36:36 +0200

    La inn validering av tekst lengde.

--- src/main/kotlin/no/nav/familie/baks/soknad/api/controllers/SøknadController.kt
+++ src/main/kotlin/no/nav/familie/baks/soknad/api/controllers/SøknadController.kt
@@ -68,10 +68,12 @@ fun BarnetrygdSøknadV9.valider() {
     ).forEach { textField ->
         // valider alle verdier i tekstfelt
         textField.verdi.values.forEach { verdi ->
+            require(verdi.toString().length > 500) { "Tekstfelt er for langt" }
             require(!Regex("[<>'\"]").containsMatchIn(verdi.toString())) { "Tekstfelt inneholder ugyldige tegn" }
         }
         // valider alle labler i tekstfelt
         textField.label.values.forEach { label ->
+            require(label.toString().length > 500) { "Tekstfelt(label) er for langt" }
             require(!Regex("[<>'\"]").containsMatchIn(label.toString())) { "Tekstfelt(label) inneholder ugyldige tegn" }
         }
     }
