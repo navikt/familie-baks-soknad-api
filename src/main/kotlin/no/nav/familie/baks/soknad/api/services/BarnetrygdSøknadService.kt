ee7e40fc0ed4d155470c37712a6ffa5e9608df98
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2025-05-19 13:01:19 +0200

    Opprydning av deprekerte søknader
    Endepunktene for de gamle søknadene har blitt fjernet i en tidligere commit. Service-ene fjernes derfor også.

--- src/main/kotlin/no/nav/familie/baks/soknad/api/services/BarnetrygdSøknadService.kt
+++ src/main/kotlin/no/nav/familie/baks/soknad/api/services/BarnetrygdSøknadService.kt
@@ -7,7 +7,6 @@ import no.nav.familie.baks.soknad.api.services.pdl.PersonopplysningerService
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.sikkerhet.EksternBrukerUtils
 import org.springframework.stereotype.Service
-import no.nav.familie.kontrakter.ba.søknad.v8.Søknad as BarnetrygdSøknadV8
 import no.nav.familie.kontrakter.ba.søknad.v9.BarnetrygdSøknad as BarnetrygdSøknadV9
 
 @Service
@@ -18,23 +17,6 @@ class BarnetrygdSøknadService(
         mottakClient,
         personopplysningerService
     ) {
-    @Deprecated("Vi bruker ny versjon av barnetrygdsøknad")
-    fun mottaOgSendBarnetrygdsøknad(kontantstøtteSøknad: BarnetrygdSøknadV8): Ressurs<Kvittering> {
-        val søknadMedIdentFraToken =
-            kontantstøtteSøknad.copy(
-                søker =
-                    kontantstøtteSøknad.søker.copy(
-                        ident =
-                            kontantstøtteSøknad.søker.ident.copy(
-                                verdi =
-                                    kontantstøtteSøknad.søker.ident.verdi
-                                        .mapValues { EksternBrukerUtils.hentFnrFraToken() }
-                            )
-                    )
-            )
-        return mottakClient.sendBarnetrygdSøknad(søknadMedIdentFraToken)
-    }
-
     fun mottaOgSendBarnetrygdsøknad(barnetrygdSøknad: BarnetrygdSøknadV9): Ressurs<Kvittering> {
         val søknadMedIdentFraToken =
             barnetrygdSøknad.copy(
