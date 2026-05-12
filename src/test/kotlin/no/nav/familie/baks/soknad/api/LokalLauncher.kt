fd650531f7f68637395a0130a74ab8a88fc093c7
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2026-05-12 23:00:02 +0200

    Legg til MockOAuth2Server for lokal kjøring

--- src/test/kotlin/no/nav/familie/baks/soknad/api/LokalLauncher.kt
+++ src/test/kotlin/no/nav/familie/baks/soknad/api/LokalLauncher.kt
@@ -6,19 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder
 @SpringBootApplication(scanBasePackages = ["no.nav.familie.baks.soknad.api"])
 class LokalLauncher
 
-/**
- * Denne settes til en fixed port for å kunne bruke samme port som familie-dokument
- */
-private const val MOCK_OAUTH2_SERVER_PORT: String = "11588"
-
 fun main(args: Array<String>) {
     SpringApplicationBuilder(LokalLauncher::class.java)
+        .initializers(MockOAuth2ServerInitializer())
         .profiles(
             "lokal",
             "mock-pdl",
             "mock-kodeverk",
             "mock-mottak",
-            "mock-kontoregister"
-        ).properties(mapOf("mock-oauth2-server.port" to MOCK_OAUTH2_SERVER_PORT))
-        .run(*args)
+            "mock-kontoregister",
+        ).run(*args)
 }
